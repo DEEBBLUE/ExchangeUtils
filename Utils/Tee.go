36d0157834cc0273c414c47a711fd8f650475b1e
Lifter <Sosite.my_dick@gmail.com>
2025-07-26 20:40:35 -0400

    Create pattern tee

---
+++ Utils/Tee.go
@@ -0,0 +1,25 @@
+package tee
+
+import "sync"
+
+func CreateTee[T any](mainCh chan T, amount int) ([]chan T,error){
+	list := make([]chan T,amount)
+	for i := range amount{
+		list[i] = make(chan T)
+	}
+	go func ()  {
+		for i := range amount {
+			defer close(list[i])
+		}
+		for val := range mainCh{
+			wg := &sync.WaitGroup{}
+			for i := range amount {
+				wg.Add(1)
+				go func ()  {
+					list[i] <- val
+				}()
+			}		
+		}	
+	}()
+	return list,nil
+}
